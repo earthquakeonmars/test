af28cc7799d8db0a5c5f6087eda275c94877c36e
Stepan Zibarov <stepanzibarov@gmail.com>
2024-11-09 23:16:25 +0300

    Modify flower
    Change extension of the flower from txt to py, change the code

---
+++ flower.py
@@ -0,0 +1,12 @@
+class Flower:
+    flowers_total: int = 0
+
+    def __init__(self, name: str):
+        self._name: str = name
+        Flower.flowers_total += 1
+
+flower1 = Flower('rose')
+flower2 = Flower('tulip')
+print(Flower.flowers_total)
+flower3 = Flower('poppy')
+print(Flower.flowers_total)
